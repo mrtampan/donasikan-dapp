543b824c819734b94529b50414b8328c76b01e79
Achmad Rivaldi <mrtampan54@gmail.com>
2025-11-17 13:49:28 +0700

    fix: Fix receive notification

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,18 +14,27 @@ const ether = useEtherStore()
 const data = ref<any>(null)
 
 async function fetchData() {
-  let web3 = await ether.getWeb3Listener()
-  let contract = await ether.getContractListener(web3)
+  let web3Listener = await ether.getWeb3Listener()
+  let contract = await ether.getContractListener(web3Listener)
+
+  let web3 = await ether.getWeb3();
+  let account = await ether.getAccount(web3)
 
   const subscription = contract.events.DonasiNote()
 
   subscription.on('data', async (event: any) => {
-      toast('Terima kasih ' + `${event.returnValues.name} telah berdonasi ${web3.utils.fromWei(event.returnValues.amount, 'ether')} POL`, {
-        description: `${event.returnValues.message}`,
-        duration: 20000,
-        position: 'top-center',
-      }
+    // Gunakan 2 account metamask & chrome untuk menguji notifikasi ini
+    if(account == event.returnValues.addressDonatur) {
+      toast(
+         'Terima kasih ' +
+          `${event.returnValues.name} telah berdonasi ${web3Listener.utils.fromWei(event.returnValues.amount, 'ether')} POL`,
+        {
+          description: `${event.returnValues.message}`,
+          duration: 20000,
+          position: 'top-center',
+        },
       )
+    }
   })
 }
 
